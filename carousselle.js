cb15d96de661cfce0e687e2901fa402dd6411a43
jimmy <jimmy@creation2sites.fr>
2023-06-14 11:55:02 +0200

    carousselle.js

--- carousselle.js
+++ carousselle.js
@@ -1,10 +1,10 @@
-var images = ["image1.jpg", "image2.jpg", "image3.jpg"]; // Ajoutez ici les noms de vos images
-var currentIndex = 0;
-var imageElement = document.getElementById("image");
+let images = ["image1.jpg", "image2.jpg", "image3.jpg"]; // Ajoutez ici les noms de vos images
+let currentIndex = 0;
+let imageElement = document.getElementById("image");
 
 function changeImage() {
   currentIndex = (currentIndex + 1) % images.length;
-  var currentImage = images[currentIndex];
+  let currentImage = images[currentIndex];
   imageElement.setAttribute("src", currentImage);
 }
 
